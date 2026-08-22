d35f2c9d2db7bcf68c5314cfffc3d3201ffb8acc
howell-game <ebukaolisaemeka@yahoo.com>
2026-08-22 16:09:55 +0100

    Add older Android browser support

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
   base: '/trendgame/',// ✅ VERY IMPORTANT for Render
@@ -11,6 +12,13 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    legacy({
+      targets: [
+        'defaults',
+        'Android >= 7',
+        'Chrome >= 60'
+      ]
+    }),
   ],
   resolve: {
     alias: {
